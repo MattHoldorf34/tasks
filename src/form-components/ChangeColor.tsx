e93502a312206b01c163444d8c6db03222204373
Matthew Holdorf <mholdorf@Matthews-MacBook-Pro-2.local>
2022-03-29 22:26:08 -0400

    Passing tests but I have a console error I can't solve.

--- src/form-components/ChangeColor.tsx
+++ src/form-components/ChangeColor.tsx
@@ -1,9 +1,48 @@
+import { cp } from "fs";
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
+
+const COLORS = [
+    "blue",
+    "red",
+    "green",
+    "orange",
+    "black",
+    "white",
+    "purple",
+    "pink"
+];
 
 export function ChangeColor(): JSX.Element {
+    const [chosen, setChosen] = useState<string>("blue");
     return (
         <div>
             <h3>Change Color</h3>
+            <div>
+                The current color is{" "}
+                <span
+                    style={{ backgroundColor: chosen }}
+                    data-testid="colored-box"
+                >
+                    {" "}
+                    {chosen}
+                </span>
+            </div>
+            {COLORS.map((color: string) => (
+                <Form.Check
+                    key={color}
+                    inline
+                    type="radio"
+                    id={"colors-choice-" + color}
+                    label={color}
+                    name="colors"
+                    value={color}
+                    checked={chosen === color}
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                        setChosen(event.target.value)
+                    }
+                />
+            ))}
         </div>
     );
 }
--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -1,13 +1,26 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
+type ChangeEvent = React.ChangeEvent<
+    HTMLTextAreaElement | HTMLInputElement | HTMLSelectElement
+>;
 export function CheckAnswer({
     expectedAnswer
 }: {
     expectedAnswer: string;
 }): JSX.Element {
+    const [userAnswer, updateAnswer] = useState<string>("");
+
+    function setAnswer(event: ChangeEvent) {
+        updateAnswer(event.target.value);
+    }
     return (
         <div>
-            <h3>Check Answer</h3>
+            <Form.Group controlId="answerChecker">
+                <Form.Label>Enter your answer:</Form.Label>
+                <Form.Control value={userAnswer} onChange={setAnswer} />
+            </Form.Group>
+            <div>{userAnswer === expectedAnswer ? "✔️" : "❌"}.</div>
         </div>
     );
 }
